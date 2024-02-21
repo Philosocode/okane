305734d820f45943beb4642e421feb81eb09d66e
Tam Le <philosocode@gmail.com>
2024-02-21 00:13:23 -0700

    3: Add API client

--- Okane.Client/src/App.vue
+++ Okane.Client/src/App.vue
@@ -1,10 +1,40 @@
 <script setup lang="ts">
+// External
+import { computed, onMounted, ref } from 'vue';
 import { VueQueryDevtools } from '@tanstack/vue-query-devtools'
+
+// Internal
+import { APIClient } from '@/shared/services/APIClient';
+
+const isHealthy = ref<boolean>()
+
+type HealthCheckResponse = {
+  status: 'Healthy' | 'Unhealthy'
+  duration: number
+}
+
+onMounted(() => {
+  APIClient.get<HealthCheckResponse>("/health")
+    .then(res => {
+      isHealthy.value = res.status === 'Healthy'
+    })
+    .catch(() => {
+      isHealthy.value = false
+    })
+})
+
+const healthMessage = computed(() => {
+  if (isHealthy.value === true) return "Healthy"
+  if (isHealthy.value === false) return "Unhealthy"
+  return "Loading..."
+})
+
 </script>
 
 <template>
   <header>
     <h1>Hello world</h1>
+    <h2>Server Health: {{ healthMessage }}</h2>
   </header>
   <VueQueryDevtools />
 </template>
---
+++ Okane.Client/src/shared/services/APIClient.ts
@@ -0,0 +1,72 @@
+// Internal
+import { isErrorWithMessage } from '@/shared/utils/errors'
+
+enum MIMEType {
+  JSON = 'application/json',
+}
+
+enum HTTPMethod {
+  GET = 'GET',
+  POST = 'POST',
+  PUT = 'PUT',
+  PATCH = 'PATCH',
+  DELETE = 'DELETE',
+}
+
+/**
+ * Custom wrapper around the Fetch API.
+ *
+ * @param endpoint
+ * @param options
+ *
+ * @see https://kentcdodds.com/blog/replace-axios-with-a-simple-custom-fetch-wrapper
+ * @see https://stackoverflow.com/a/65690669
+ */
+async function APIClient<TResponse>(endpoint: string, options?: RequestInit): Promise<TResponse> {
+  const { body, ...customConfig } = options ?? {}
+
+  const config: RequestInit = {
+    method: body ? 'POST' : 'GET',
+    ...customConfig,
+    headers: {
+      'Content-Type': MIMEType.JSON,
+      ...customConfig.headers,
+    },
+  }
+
+  if (body) config.body = JSON.stringify(body)
+
+  let data
+  try {
+    const url = `/api/${formatEndpoint(endpoint)}`
+    const response = await window.fetch(url, config)
+
+    data = await response.json()
+
+    if (response.ok) return data
+    throw new Error(response.statusText)
+  } catch (err) {
+    return Promise.reject(isErrorWithMessage(err) ? err.message : data)
+  }
+}
+
+function formatEndpoint(endpoint: string) {
+  return endpoint.startsWith('/') ? endpoint.substring(1) : endpoint
+}
+
+APIClient.get = <TResponse>(endpoint: string, customConfig?: RequestInit) =>
+  APIClient<TResponse>(endpoint, { ...customConfig, method: HTTPMethod.GET })
+
+APIClient.post = <TResponse>(endpoint: string, body: BodyInit | null, customConfig?: RequestInit) =>
+  APIClient<TResponse>(endpoint, { ...customConfig, body, method: HTTPMethod.POST })
+
+APIClient.put = <TResponse>(endpoint: string, body: BodyInit | null, customConfig?: RequestInit) =>
+  APIClient<TResponse>(endpoint, { ...customConfig, body, method: HTTPMethod.PUT })
+
+APIClient.patch = <TResponse>(endpoint: string, body: BodyInit | null, customConfig?: RequestInit) =>
+  APIClient<TResponse>(endpoint, { ...customConfig, body, method: HTTPMethod.PATCH })
+
+APIClient.delete = <TResponse>(endpoint: string, customConfig?: RequestInit) =>
+  APIClient<TResponse>(endpoint, { ...customConfig, method: HTTPMethod.DELETE })
+
+export { APIClient }

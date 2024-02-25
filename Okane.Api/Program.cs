c6057b6ee7b1be8efeec0c83c7d8cb6bfc4f0d44
Tam Le <philosocode@gmail.com>
2024-02-24 22:04:18 -0700

    1-API: Set up Identity

--- Okane.Api/Program.cs
+++ Okane.Api/Program.cs
@@ -1,7 +1,9 @@
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Okane.Api.Features.Auth.Models;
 using Okane.Api.Infrastructure.AppSettings;
 using Okane.Api.Infrastructure.Database;
 using Okane.Api.Infrastructure.HealthCheck;
+using Okane.Api.Shared.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +12,23 @@ builder.AddAppSettingsJsonFiles();
 builder.Services.Configure<DbSettings>(builder.Configuration.GetSection(nameof(DbSettings)));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddDbContext<ApiDbContext>();
+
+builder.Services.AddAuthorization();
+builder.Services.AddIdentityApiEndpoints<ApiUser>(options => 
+    {
+        options.Password.RequireDigit = true;
+        options.Password.RequireLowercase = true;
+        options.Password.RequireUppercase = true;
+        options.Password.RequireNonAlphanumeric = true;
+        options.Password.RequiredLength = 12; // Min length
+        
+        // TODO #16: Users need to confirm their account before they can login.
+        // options.SignIn.RequireConfirmedAccount = true;
+
+        options.Lockout.AllowedForNewUsers = true; 
+    })
+    .AddEntityFrameworkStores<ApiDbContext>();
+
 builder.Services.AddHealthChecks().AddDbContextCheck<ApiDbContext>();
 builder.Services.AddSwaggerGen();
 
@@ -29,9 +48,13 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthorization();
+
 app.UseHealthChecks("/api/health", new HealthCheckOptions()
 {
     ResponseWriter = HealthCheckResponseWriter.WriteResponse
 });
+
+app.MapApiEndpoints();
 
 app.Run();

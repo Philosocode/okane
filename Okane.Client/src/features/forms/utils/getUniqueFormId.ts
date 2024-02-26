cf576374cccb70e43a8d68de1e4474495e8bbef8
Tam Le <philosocode@gmail.com>
2024-02-25 23:48:06 -0700

    1-FE: Add basic register form

---
+++ Okane.Client/src/features/forms/utils/getUniqueFormId.ts
@@ -0,0 +1,11 @@
+// External
+import { ref } from 'vue'
+
+const formIdRef = ref(0);
+
+// Return a unique value that can be used for a form control.
+export function getUniqueFormId() {
+  formIdRef.value++
+  return String(formIdRef.value)
+}
+

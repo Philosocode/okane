f068f0d71710503e269a8a0916f8e36baa46b4c7
Tam Le <philosocode@gmail.com>
2024-03-16 17:51:39 -0600

    1-API: Add refresh token entity

---
+++ Okane.Api/Features/Auth/Entities/ApiUser.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Identity;
+
+namespace Okane.Api.Features.Auth.Entities;
+
+public class ApiUser : IdentityUser
+{
+    [MaxLength(100)]
+    public required string Name { get; set; }
+    
+    [JsonIgnore]
+    public ICollection<RefreshToken> RefreshTokens { get; set; }  = default!;
+}
--- Okane.Api/Features/Auth/Models/ApiUser.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.AspNetCore.Identity;
-
-namespace Okane.Api.Features.Auth.Models;
-
-public class ApiUser : IdentityUser
-{
-    public string Name { get; set; }
-}

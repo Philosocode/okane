c9c21a16bcb27474f6dcb0a56c0606be142c90d3
Tam Le <philosocode@gmail.com>
2024-03-16 19:23:33 -0600

    1-API: Add standardized API response DTOs

--- Okane.Api/Features/Auth/Dtos/Requests/RegisterRequest.cs
+++ Okane.Api/Features/Auth/Dtos/Requests/RegisterRequest.cs
@@ -1,10 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Okane.Api.Features.Auth.Dtos.Requests;
 
 public record RegisterRequest
 {
+    [Required]
     public required string Name { get; set; }
     
+    [EmailAddress]
     public required string Email { get; set; }
     
+    [Required]
     public required string Password { get; set; }
 }

d702cfb45ca06e9ef96dcdf326d29f63ed3efdbf
Tam Le <philosocode@gmail.com>
2024-03-18 21:27:33 -0600

    1-FE: Combine register and login form

---
+++ Okane.Client/src/features/auth/AuthForm.vue
@@ -0,0 +1,82 @@
+<script setup lang="ts">
+// External
+import { computed, ref } from 'vue'
+
+// Internal
+import FormInput from '@/features/forms/FormInput.vue'
+
+import type { AuthFormState, AuthFormType } from '@/features/auth/auth.types'
+import { isValidPassword } from '@/features/auth/auth.utils'
+
+const emit = defineEmits<{
+  (e: 'submit', formState: AuthFormState): void
+}>()
+
+const props = defineProps<{ formType: AuthFormType }>()
+
+const formState = ref({
+  email: '',
+  name: '',
+  password: '',
+  passwordConfirm: '',
+})
+
+const isRegisterForm = computed<boolean>(() => props.formType == 'register')
+const submitButtonText = computed<string>(() => {
+  if (isRegisterForm.value) return 'Register'
+  return 'Login'
+})
+
+const formIsValid = computed<boolean>(() => {
+  const { email, name, password, passwordConfirm } = formState.value
+
+  const validations = [Boolean(email), Boolean(password)]
+
+  if (isRegisterForm.value) {
+    validations.push(
+      Boolean(name),
+      Boolean(passwordConfirm),
+      password == passwordConfirm,
+      isValidPassword(password)[0],
+    )
+  }
+
+  return !validations.includes(false)
+})
+</script>
+
+<template>
+  <form @submit.prevent="emit('submit', formState)">
+    <fieldset>
+      <FormInput
+        v-if="isRegisterForm"
+        v-model="formState.name"
+        label="Name"
+        name="name"
+        type="text"
+      />
+
+      <FormInput label="Email" name="email" type="email" v-model="formState.email" />
+      <FormInput label="Password" name="password" type="password" v-model="formState.password" />
+
+      <FormInput
+        v-if="isRegisterForm"
+        v-model="formState.passwordConfirm"
+        label="Confirm password"
+        name="passwordConfirm"
+        type="password"
+      />
+
+      <button :disabled="!formIsValid" type="submit">{{ submitButtonText }}</button>
+    </fieldset>
+  </form>
+</template>
+
+<style scoped>
+fieldset {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  max-width: 12rem;
+}
+</style>

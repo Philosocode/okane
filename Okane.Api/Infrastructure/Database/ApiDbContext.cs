ca2307bd8686c2a1e2b5c37bebd9a1b256977458
Tam Le <philosocode@gmail.com>
2024-03-20 19:28:15 -0600

    1-API: DbContext: Add `OnModelCreating` override

--- Okane.Api/Infrastructure/Database/ApiDbContext.cs
+++ Okane.Api/Infrastructure/Database/ApiDbContext.cs
@@ -8,11 +8,16 @@ namespace Okane.Api.Infrastructure.Database;
 public class ApiDbContext(IOptions<DbSettings> options) : IdentityDbContext<ApiUser>
 {
     public DbSet<RefreshToken> RefreshTokens { get; set; } = default!;
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.ApplyConfigurationsFromAssembly(typeof(ApiDbContext).Assembly);
+    }
     
     protected override void OnConfiguring(DbContextOptionsBuilder builder)
     {
         base.OnConfiguring(builder);
-        
         builder.UseNpgsql(options.Value.ConnectionString);
     }
 }

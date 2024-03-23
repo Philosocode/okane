6d2ee8991982da582331efce33174943709f134a
Tam Le <philosocode@gmail.com>
2024-03-23 10:48:10 -0600

    1-API: Add `Id` to `RefreshToken` entity
    In the future, this will be used in endpoint filters.

--- Okane.Api/Infrastructure/Database/Entities/IOwnedEntity.cs
+++ Okane.Api/Infrastructure/Database/Entities/IOwnedEntity.cs
@@ -3,7 +3,7 @@ namespace Okane.Api.Infrastructure.Database.Entities;
 /// <summary>
 /// Entity that can be owned by a User.
 /// </summary>
-public interface IOwnedEntity
+public interface IOwnedEntity : IEntity
 {
     string UserId { get; }
 }

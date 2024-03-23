4d7170a2732f2631839ca883b4fa7e544b56c3e0
Tam Le <philosocode@gmail.com>
2024-03-23 11:51:01 -0600

    26-FE: Set up testing deps, including MSW

---
+++ Okane.Client/src/__tests__/setUpTests.ts
@@ -0,0 +1,14 @@
+// Internal
+import { server } from '@/tests/msw/server'
+
+beforeAll(() => {
+  server.listen({ onUnhandledRequest: 'error' })
+})
+
+afterEach(() => {
+  server.resetHandlers()
+})
+
+afterAll(() => {
+  server.close()
+})

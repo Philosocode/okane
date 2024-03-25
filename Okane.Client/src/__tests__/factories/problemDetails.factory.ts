ecda207629b5948562e206e729590c78eee28e49
Tam Le <philosocode@gmail.com>
2024-03-24 18:45:35 -0600

    26-FE: Fix factories not working properly

--- Okane.Client/src/__tests__/factories/problemDetails.factory.ts
+++ Okane.Client/src/__tests__/factories/problemDetails.factory.ts
@@ -3,7 +3,7 @@ import { HTTPStatusCode } from '@/shared/constants/http.constants'
 
 import type { ProblemDetails } from '@/shared/services/apiClient/apiClient.types'
 
-import { baseMockFactory } from '@tests/factories/base.factory'
+import { baseMockFactory, type MockFactoryFunction } from '@tests/factories/base.factory'
 
 const defaultProblemDetails: ProblemDetails = {
   detail: 'An exception occurred while editing a finance record.',
@@ -13,6 +13,9 @@ const defaultProblemDetails: ProblemDetails = {
   type: 'https://datatracker.ietf.org/doc/html/rfc9110#section-15.5.1',
 }
 
-export function createMockProblemDetails() {
-  return baseMockFactory.bind(null, defaultProblemDetails)
+export const createMockProblemDetails: MockFactoryFunction<ProblemDetails> = (
+  overrides,
+  options,
+) => {
+  return baseMockFactory(defaultProblemDetails, overrides, options)
 }

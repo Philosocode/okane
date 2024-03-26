a18155534375ad476b107c80160f275959c63d41
Tam Le <philosocode@gmail.com>
2024-03-26 01:34:31 -0600

    26-FE: Add `DashboardPage` tests

---
+++ Okane.Client/src/shared/pages/DashboardPage.spec.ts
@@ -0,0 +1,16 @@
+// Internal
+import DashboardPage from '@/shared/pages/DashboardPage.vue'
+
+const mountComponent = getMountComponent(DashboardPage)
+
+test('renders a Dashboard heading', () => {
+  const wrapper = mountComponent()
+  const mainHeading = wrapper.get('h1')
+  expect(mainHeading.text()).toBe('Dashboard')
+})
+
+test('renders the logged in text', () => {
+  const wrapper = mountComponent()
+  const loggedInText = wrapper.get('p')
+  expect(loggedInText.text()).toBe('You are logged in!')
+})

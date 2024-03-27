dff768586a366ecc3afdc5239aef32f60bfb5873
Tam Le <philosocode@gmail.com>
2024-03-26 20:29:42 -0600

    26-FE: Create auth form state factory

---
+++ Okane.Client/src/__tests__/factories/authFormState.factory.ts
@@ -0,0 +1,15 @@
+// Internal
+import type { AuthFormState } from 'src/features/auth/auth.types'
+
+import { baseMockFactory, type MockFactoryFunction } from '@tests/factories/base.factory'
+
+const defaultState: AuthFormState = {
+  email: 'test@okane.com',
+  name: 'Okane',
+  password: 'coolPassword123',
+  passwordConfirm: 'coolPassword123',
+}
+
+export const createMockAuthFormState: MockFactoryFunction<AuthFormState> = (overrides, options) => {
+  return baseMockFactory(defaultState, overrides, options)
+}

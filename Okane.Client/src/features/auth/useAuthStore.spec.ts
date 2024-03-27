33760390a2cfa529bccd31ad4743a7d5bb723561
Tam Le <philosocode@gmail.com>
2024-03-26 21:58:34 -0600

    26-FE: Add `useAuthStore` tests

---
+++ Okane.Client/src/features/auth/useAuthStore.spec.ts
@@ -0,0 +1,81 @@
+// External
+import { http, HttpResponse } from 'msw'
+
+// Internal
+import { HTTP_STATUS_CODE } from '@/shared/constants/http.constants'
+
+import type { AuthenticateResponse } from '@/features/auth/auth.types'
+
+import { useAuthStore } from '@/features/auth/useAuthStore'
+
+import { apiClient } from '@/shared/services/apiClient/apiClient.service'
+import { mockServer } from '@tests/msw/mockServer'
+
+import { createMockAuthFormState } from '@tests/factories/authFormState.factory'
+import { createMockJWTToken } from '@tests/factories/jwtToken.factory'
+import { createMockUser } from '@tests/factories/user.factory'
+import { omitObjectKeys } from '@/shared/utils/object.utils'
+import { wrapInAPIResponse } from '@tests/factories/apiResponse.factory'
+
+const formData = createMockAuthFormState()
+const authResponse: AuthenticateResponse = {
+  items: [{ jwtToken: createMockJWTToken(), user: createMockUser() }],
+  status: HTTP_STATUS_CODE.OK_200,
+}
+
+beforeEach(() => {
+  mockServer.use(
+    http.post('/api/auth/login', () => HttpResponse.json(authResponse)),
+    http.post('/api/auth/refresh-token', () => HttpResponse.json(authResponse)),
+    http.post('/api/auth/logout', () => HttpResponse.json(wrapInAPIResponse(null))),
+  )
+})
+
+afterEach(() => {
+  vi.restoreAllMocks()
+})
+
+test('register', async () => {
+  const spy = vi.spyOn(apiClient, 'post').mockResolvedValue(wrapInAPIResponse(null))
+
+  const authStore = useAuthStore()
+  await authStore.register(formData.email, formData.name, formData.password)
+
+  expect(spy).toHaveBeenCalledTimes(1)
+  expect(spy).toHaveBeenCalledWith('/auth/register', omitObjectKeys(formData, ['passwordConfirm']))
+})
+
+test('login', async () => {
+  const authStore = useAuthStore()
+  await authStore.login(formData.email, formData.password)
+
+  expect(authStore.authUser).toEqual(authResponse.items[0].user)
+  expect(authStore.jwtToken).toEqual(authResponse.items[0].jwtToken)
+
+  // Need to manually log out after each test to clear the timeout.
+  await authStore.logout()
+})
+
+test('handleRefreshToken', async () => {
+  const authStore = useAuthStore()
+  await authStore.handleRefreshToken()
+
+  expect(authStore.authUser).toEqual(authResponse.items[0].user)
+  expect(authStore.jwtToken).toEqual(authResponse.items[0].jwtToken)
+
+  // Need to manually log out after each test to clear the timeout.
+  await authStore.logout()
+})
+
+test('logout', async () => {
+  const authStore = useAuthStore()
+  authStore.$patch({
+    authUser: createMockUser(),
+    jwtToken: createMockJWTToken(),
+  })
+
+  await authStore.logout()
+
+  expect(authStore.authUser).toBeUndefined()
+  expect(authStore.jwtToken).toBeUndefined()
+})

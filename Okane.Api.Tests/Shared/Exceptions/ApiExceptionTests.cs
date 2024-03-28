f16e2c033c1b72345f4a7a4f4b481709ac1c8c19
Tam Le <philosocode@gmail.com>
2024-03-27 21:12:26 -0600

    26-API: Add `ApiException` tests

---
+++ Okane.Api.Tests/Shared/Exceptions/ApiExceptionTests.cs
@@ -0,0 +1,32 @@
+using System.Net;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Okane.Api.Shared.Exceptions;
+
+namespace Okane.Api.Tests.Shared.Exceptions;
+
+public class ApiExceptionTests
+{
+    [Fact]
+    public void ToProblemDetails_ReturnsProblemDetails()
+    {
+        string message = "An error occurred";
+        var exception = new ApiException(message)
+        {
+            Status = HttpStatusCode.Accepted,
+            Instance = "Cool Instance",
+            Title = "Cool Title",
+        };
+
+        var actual = exception.ToProblemDetails();
+        var expected = new ProblemDetails
+        {
+            Detail = message,
+            Instance = exception.Instance,
+            Status = (int)exception.Status,
+            Title = exception.Title,
+        };
+        
+        actual.Should().BeEquivalentTo(expected);
+    }
+}

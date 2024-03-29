a8ffa6c3980404f74c1390507db5c8433d95b46c
Tam Le <philosocode@gmail.com>
2024-03-28 21:03:46 -0600

    26-API: Add auth mappers tests

---
+++ Okane.Api.Tests/Features/Auth/Mappers/UserMappersTests.cs
@@ -0,0 +1,29 @@
+using FluentAssertions;
+using Okane.Api.Features.Auth.Dtos.Responses;
+using Okane.Api.Features.Auth.Entities;
+using Okane.Api.Features.Auth.Mappers;
+
+namespace Okane.Api.Tests.Features.Auth.Mappers;
+
+public class UserMappersTests
+{
+    [Fact]
+    public void ToUserResponse_ReturnsAUserResponse()
+    {
+        var apiUser = new ApiUser
+        {
+            Id = "okane-user-id",
+            Name = "Okane",
+            Email = "test@okane.com",
+        };
+
+        var actual = apiUser.ToUserResponse();
+        var expected = new UserResponse
+        {
+            Email = apiUser.Email,
+            Name = apiUser.Name,
+        };
+
+        actual.Should().Be(expected);
+    }
+}

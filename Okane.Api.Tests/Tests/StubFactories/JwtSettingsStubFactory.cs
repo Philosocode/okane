d1dcab2a9638720d2c351d70ccae0b8eb1ab0d94
Tam Le <philosocode@gmail.com>
2024-03-29 13:03:25 -0600

    26-API: Add `JwtSettingsStubFactory`

---
+++ Okane.Api.Tests/Tests/StubFactories/JwtSettingsStubFactory.cs
@@ -0,0 +1,19 @@
+using Bogus;
+using Okane.Api.Features.Auth.Config;
+
+namespace Okane.Api.Tests.Tests.StubFactories;
+
+public class JwtSettingsStubFactory : IStubFactory<JwtSettings>
+{
+    public static Faker<JwtSettings> GetFactory()
+    {
+        return new Faker<JwtSettings>()
+            .RuleFor(x => x.Audience, "https://client.philosocode.com")
+            .RuleFor(x => x.Issuer, "https://api.philosocode.com")
+            .RuleFor(x => x.IssuerSigningKey, new Faker().Random.String2(length: 100))
+            .RuleFor(x => x.MinutesToExpiration, 540)
+            .RuleFor(x => x.RefreshTokenTtlDays, 3);
+    }
+
+    public static JwtSettings Create() => GetFactory().Generate();
+}

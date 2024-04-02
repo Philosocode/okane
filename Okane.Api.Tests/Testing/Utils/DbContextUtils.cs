3ee3cef1652a1ace56ee48c26c160113c203d46c
Tam Le <philosocode@gmail.com>
2024-04-02 00:33:55 -0600

    26-API: Rename `DbTestingUtils` to `DbContextUtils`

--- Okane.Api.Tests/Testing/Utils/DbContextUtils.cs
+++ Okane.Api.Tests/Testing/Utils/DbContextUtils.cs
@@ -4,9 +4,13 @@ using Okane.Api.Tests.Testing.StubFactories;
 
 namespace Okane.Api.Tests.Testing.Utils;
 
-public static class DbTestingUtils
+/// <summary>
+/// Utils for working with a standalone DbContext. These are intended for use when directly
+/// injecting an ApiDbContext as opposed to using a WebApplicationFactory.
+/// </summary>
+public static class DbContextUtils
 {
-    public static ApiUser InsertApiUser(ApiDbContext db)
+    public static ApiUser AddApiUser(ApiDbContext db)
     {
         var apiUser = ApiUserStubFactory.Create();
         

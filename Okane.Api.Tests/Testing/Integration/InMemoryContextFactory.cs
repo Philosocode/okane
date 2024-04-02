46d340340b3d611dc8f551b36964732f2192623d
Tam Le <philosocode@gmail.com>
2024-04-02 02:15:06 -0600

    26-API: Update refresh tokens migration to set `CreatedAt` default value

--- Okane.Api.Tests/Testing/Integration/InMemoryContextFactory.cs
+++ Okane.Api.Tests/Testing/Integration/InMemoryContextFactory.cs
@@ -1,4 +1,3 @@
-using System.Data.Common;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using Okane.Api.Infrastructure.Database;
@@ -8,11 +7,12 @@ namespace Okane.Api.Tests.Testing.Integration;
 // Referenced: https://stackoverflow.com/q/67174971
 public class InMemoryContextFactory : IDisposable
 {
-    private readonly DbConnection _connection;
+    private readonly SqliteConnection _connection;
 
     public InMemoryContextFactory()
     {
         _connection = new SqliteConnection("DataSource=:memory:");
+        _connection.CreateFunction("now", () => DateTime.UtcNow);
         _connection.Open();
 
         var options = CreateOptions();
--- Okane.Api/Features/Auth/Entities/RefreshToken.cs
+++ Okane.Api/Features/Auth/Entities/RefreshToken.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Okane.Api.Infrastructure.Database.Entities;
 
 namespace Okane.Api.Features.Auth.Entities;
@@ -9,7 +10,7 @@ public class RefreshToken : IOwnedEntity
     public int Id { get; set; }
     public required string Token { get; set; }
     public required DateTime ExpiresAt { get; set; }
-    public required DateTime CreatedAt { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public DateTime? RevokedAt { get; set; }
     
     // Navigation.
@@ -20,4 +21,14 @@ public class RefreshToken : IOwnedEntity
     public bool IsExpired => DateTime.UtcNow >= ExpiresAt;
     public bool IsRevoked => RevokedAt != null;
     public bool IsActive => !IsExpired && !IsRevoked;
+}
+
+public class RefreshTokenEntityConfiguration : IEntityTypeConfiguration<RefreshToken>
+{
+    public void Configure(EntityTypeBuilder<RefreshToken> builder)
+    {
+        builder
+            .Property("CreatedAt").HasDefaultValueSql("NOW()");
+        
+    }
 }

4b83ce43431160110f13c32b66155cde8ca7b2c5
Tam Le <philosocode@gmail.com>
2024-04-02 00:34:15 -0600

    26-API: Add `RefreshTokenCleanupService` tests

---
+++ Okane.Api.Tests/Infrastructure/HostedServices/RefreshTokenCleanupServiceTests.cs
@@ -0,0 +1,76 @@
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+using Okane.Api.Features.Auth.Entities;
+using Okane.Api.Infrastructure.HostedServices;
+using Okane.Api.Shared.Wrappers;
+using Okane.Api.Tests.Testing.Integration;
+using Okane.Api.Tests.Testing.Mocks.Wrappers;
+using Okane.Api.Tests.Testing.Utils;
+
+namespace Okane.Api.Tests.Infrastructure.HostedServices;
+
+public class RefreshTokenCleanupServiceTests
+{
+    private readonly IClock _clock = new TestingClock();
+    private readonly InMemoryContextFactory _contextFactory = new InMemoryContextFactory();
+
+    private readonly ILogger<RefreshTokenCleaner> _logger = Substitute.For<ILogger<RefreshTokenCleaner>>();
+
+    [Fact]
+    public async Task RemovesExpiredAndRevokedTokens()
+    {
+        var db = _contextFactory.CreateContext();
+        var user = DbContextUtils.AddApiUser(db);
+
+        var expiredToken = new RefreshToken
+        {
+            Token = Guid.NewGuid().ToString(),
+            UserId = user.Id,
+            CreatedAt = default,
+            ExpiresAt = _clock.UtcNow.AddSeconds(-1),
+        };
+        
+        var revokedToken = new RefreshToken
+        {
+            Token = Guid.NewGuid().ToString(),
+            UserId = user.Id,
+            CreatedAt = default,
+            ExpiresAt = _clock.UtcNow.AddDays(1),
+            RevokedAt = _clock.UtcNow.AddSeconds(-1)
+        };
+
+        var revokedAndExpiredToken = new RefreshToken
+        {
+            Token = Guid.NewGuid().ToString(),
+            UserId = user.Id,
+            CreatedAt = default,
+            ExpiresAt = expiredToken.ExpiresAt,
+            RevokedAt = revokedToken.RevokedAt
+        };
+
+        var validToken = new RefreshToken
+        {
+            Token = Guid.NewGuid().ToString(),
+            UserId = user.Id,
+            CreatedAt = default,
+            ExpiresAt = _clock.UtcNow.AddDays(1)
+        };
+
+        await db.AddRangeAsync([
+            expiredToken,
+            revokedToken,
+            revokedAndExpiredToken,
+            validToken
+        ]);
+        
+        await db.SaveChangesAsync();
+
+        var cleaner = new RefreshTokenCleaner(_clock, db, _logger);
+        await cleaner.ExecuteAsync(CancellationToken.None);
+
+        var remainingRefreshTokens = await db.RefreshTokens.ToListAsync();
+        remainingRefreshTokens.Should().ContainSingle(t => t.Token == validToken.Token);
+    }
+}

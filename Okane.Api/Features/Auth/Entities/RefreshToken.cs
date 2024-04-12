351c73872b87793e00234eeb89f811603f9770b6
Tam Le <philosocode@gmail.com>
2024-04-11 21:54:47 -0600

    36-API: Add `FinanceRecords` table

--- Okane.Api/Features/Auth/Entities/RefreshToken.cs
+++ Okane.Api/Features/Auth/Entities/RefreshToken.cs
@@ -7,19 +7,19 @@ namespace Okane.Api.Features.Auth.Entities;
 [Index(nameof(Token), IsUnique = true)]
 public class RefreshToken : IOwnedEntity
 {
+    public int Id { get; set; }
     public required string Token { get; set; }
     public required DateTime ExpiresAt { get; set; }
     public DateTime? RevokedAt { get; set; }
-    public ApiUser User { get; set; } = default!;
 
     // Computed.
     public bool IsExpired => DateTime.UtcNow >= ExpiresAt;
     public bool IsRevoked => RevokedAt != null;
     public bool IsActive => !IsExpired && !IsRevoked;
-    public int Id { get; set; }
 
     // Navigation.
     public string UserId { get; set; } = string.Empty;
+    public ApiUser User { get; set; } = default!;
 }
 
 public class RefreshTokenEntityConfiguration : IEntityTypeConfiguration<RefreshToken>
---
+++ Okane.Api/Infrastructure/Database/Migrations/20240412040544_AddFinanceRecords.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Okane.Api.Infrastructure.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFinanceRecords : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FinanceRecords",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Amount = table.Column<decimal>(type: "numeric", nullable: false),
+                    Description = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: false),
+                    HappenedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UserId = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FinanceRecords", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_FinanceRecords_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FinanceRecords_UserId",
+                table: "FinanceRecords",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FinanceRecords");
+        }
+    }
+}

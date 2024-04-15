1cdb7eb6924dfb6254267631c864da91febd83ea
Tam Le <philosocode@gmail.com>
2024-04-15 01:39:42 -0600

    36-API: Remove `IStubFactory` interface
    It was proving to be more limiting than helpful.

--- Okane.Api.Tests/Testing/StubFactories/IStubFactory.cs
+++
@@ -1,9 +0,0 @@
-using Bogus;
-
-namespace Okane.Api.Tests.Testing.StubFactories;
-
-public interface IStubFactory<T> where T : class
-{
-    static abstract Faker<T> GetFactory();
-    static abstract T Create();
-}
--- Okane.Api.Tests/Testing/StubFactories/JwtSettingsStubFactory.cs
+++ Okane.Api.Tests/Testing/StubFactories/JwtSettingsStubFactory.cs
@@ -3,20 +3,16 @@ using Okane.Api.Features.Auth.Config;
 
 namespace Okane.Api.Tests.Testing.StubFactories;
 
-public class JwtSettingsStubFactory : IStubFactory<JwtSettings>
+public class JwtSettingsStubFactory
 {
-    public static Faker<JwtSettings> GetFactory()
+    public static JwtSettings Create()
     {
         return new Faker<JwtSettings>()
             .RuleFor(x => x.Audience, "https://client.philosocode.com")
             .RuleFor(x => x.Issuer, "https://api.philosocode.com")
             .RuleFor(x => x.IssuerSigningKey, new Faker().Random.String2(100))
             .RuleFor(x => x.MinutesToExpiration, 540)
-            .RuleFor(x => x.RefreshTokenTtlDays, 3);
-    }
-
-    public static JwtSettings Create()
-    {
-        return GetFactory().Generate();
+            .RuleFor(x => x.RefreshTokenTtlDays, 3)
+            .Generate();
     }
 }

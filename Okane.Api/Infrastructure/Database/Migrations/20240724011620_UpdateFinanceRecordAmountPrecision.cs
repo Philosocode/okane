9269fd8c281605b744163a9c3fb241ed60c50813
Tam Le <philosocode@gmail.com>
2024-07-23 19:16:39 -0600

    38-API: Add migration for FinanceRecord.Amount precision

---
+++ Okane.Api/Infrastructure/Database/Migrations/20240724011620_UpdateFinanceRecordAmountPrecision.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Okane.Api.Infrastructure.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateFinanceRecordAmountPrecision : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Amount",
+                table: "FinanceRecords",
+                type: "numeric(9,2)",
+                precision: 9,
+                scale: 2,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Amount",
+                table: "FinanceRecords",
+                type: "numeric",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(9,2)",
+                oldPrecision: 9,
+                oldScale: 2);
+        }
+    }
+}

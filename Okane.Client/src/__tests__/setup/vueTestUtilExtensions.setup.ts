a54958e53a0dbb2ea4af22c28f5bc77c6786a446
Tam Le <philosocode@gmail.com>
2024-07-23 19:43:47 -0600

    38-FE: Fix linting error for Vue test utils plugin

--- Okane.Client/src/__tests__/setup/vueTestUtilExtensions.setup.ts
+++ Okane.Client/src/__tests__/setup/vueTestUtilExtensions.setup.ts
@@ -4,14 +4,10 @@ import { config, DOMWrapper, VueWrapper } from '@vue/test-utils'
 function plugin(wrapper: VueWrapper) {
   return {
     findAllByText(selector: string, text: string) {
-      return wrapper
-        .findAll(selector)
-        .filter((node) => node.text().includes(text))
+      return wrapper.findAll(selector).filter((node) => node.text().includes(text))
     },
     findByText(selector: string, text: string) {
-      return wrapper
-        .findAll(selector)
-        .find((node) => node.text().includes(text))
+      return wrapper.findAll(selector).find((node) => node.text().includes(text))
     },
   }
 }
@@ -19,11 +15,8 @@ function plugin(wrapper: VueWrapper) {
 config.plugins.VueWrapper.install(plugin)
 
 declare module '@vue/test-utils' {
-  export class VueWrapper {
-    findAllByText<T extends Node>(
-      element: string,
-      text: string,
-    ): DOMWrapper<T>[]
+  export interface VueWrapper {
+    findAllByText<T extends Node>(element: string, text: string): DOMWrapper<T>[]
     findByText<T extends Node>(element: string, text: string): DOMWrapper<T>
   }
 }

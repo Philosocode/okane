11ae672966a770094a5b6c2747f2312cde12796a
Tam Le <philosocode@gmail.com>
2024-08-27 19:02:12 -0600

    44-FE: Fix failing SaveFinanceRecordForm tests

--- Okane.Client/src/__tests__/spies/date.spies.ts
+++
@@ -1,8 +0,0 @@
-export const spyOnDate = {
-  now() {
-    const mockedNow = 540720900
-    const spy = vi.spyOn(Date, 'now').mockReturnValue(mockedNow)
-
-    return { spy, now: mockedNow }
-  },
-}

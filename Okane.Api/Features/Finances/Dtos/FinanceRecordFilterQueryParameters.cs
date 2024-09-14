5cdf6e5f51c3c18917a0ff612d3cf1c2f0773e13
Tam Le <philosocode@gmail.com>
2024-09-13 19:14:56 -0600

    39-API: Add `FinanceRecordFilterQueryParameters`

---
+++ Okane.Api/Features/Finances/Dtos/FinanceRecordFilterQueryParameters.cs
@@ -0,0 +1,16 @@
+using Okane.Api.Features.Finances.Entities;
+
+namespace Okane.Api.Features.Finances.Dtos;
+
+public class FinanceRecordFilterQueryParameters
+{
+    public string? Description { get; set; }
+
+    public DateTime? HappenedBefore { get; set; }
+    public DateTime? HappenedAfter { get; set; }
+
+    public decimal? MinAmount { get; set; }
+    public decimal? MaxAmount { get; set; }
+
+    public FinanceRecordType? Type { get; set; }
+}

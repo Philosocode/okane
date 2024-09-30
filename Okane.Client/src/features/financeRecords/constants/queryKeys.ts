0ed7a786e119e0aea3a8d5d47dbdc214ec7f6fc3
Tam Le <philosocode@gmail.com>
2024-09-30 16:59:51 -0600

    72-FE: Remove unneeded word from query key

--- Okane.Client/src/features/financeRecords/constants/queryKeys.ts
+++ Okane.Client/src/features/financeRecords/constants/queryKeys.ts
@@ -1,11 +1,10 @@
 // Internal
-
-import type { FinanceRecordsSearchFilters } from '@features/financeRecords/types/searchFilters'
+import { type FinanceRecordsSearchFilters } from '@features/financeRecords/types/searchFilters'
 
 export const queryKeys = {
   all: () => ['all'],
   lists: () => [...queryKeys.all(), 'lists'],
-  listByFilters: (filters: FinanceRecordsSearchFilters) => [...queryKeys.lists(), 'list', filters],
+  listByFilters: (filters: FinanceRecordsSearchFilters) => [...queryKeys.lists(), filters],
 }
 
 export const financeRecordQueryKeys = queryKeys

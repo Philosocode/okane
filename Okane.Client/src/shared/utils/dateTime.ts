c2bb38a9c2eb83a6c7b6e5311f95921c29070df1
Tam Le <philosocode@gmail.com>
2024-09-30 17:07:24 -0600

    72-FE: Add `dateOnlyTimestamp` and `mapUTCTimestampToLocalDate` utils

--- Okane.Client/src/shared/utils/dateTime.ts
+++ Okane.Client/src/shared/utils/dateTime.ts
@@ -11,6 +11,28 @@ export const mapDate = createMappers({
    * @param d {Date}
    */
   dateTimeLocal(d: Date) {
-    return format(d, "yyyy-MM-dd'T'kk:mm")
+    return format(d, `yyyy-MM-dd'T'kk:mm`)
+  },
+  dateOnlyTimestamp(d: Date) {
+    return format(d, 'yyyy-MM-dd')
   },
 })
+
+/**
+ * Convert a UTC timestamp to a date.
+ *
+ * @param timestamp
+ */
+export function mapUTCTimestampToLocalDate(timestamp: string): Date {
+  const d = new Date(timestamp)
+
+  return new Date(
+    d.getUTCFullYear(),
+    d.getUTCMonth(),
+    d.getUTCDate(),
+    d.getUTCHours(),
+    d.getUTCMinutes(),
+    d.getUTCSeconds(),
+    d.getUTCMilliseconds(),
+  )
+}

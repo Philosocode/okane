85c4e0f3acb54501e0ef89293eb0bed6b8c41982
Tam Le <philosocode@gmail.com>
2024-10-28 19:11:47 -0600

    16-FE: Update finance record root query key value
    Wasn't accounting for the fact that there'll be other features with
    separate query key constants.

--- Okane.Client/src/features/financeRecords/constants/queryKeys.ts
+++ Okane.Client/src/features/financeRecords/constants/queryKeys.ts
@@ -2,10 +2,17 @@
 import { type FinanceRecordsSearchFilters } from '@features/financeRecords/types/searchFinanceRecords'
 
 export const queryKeys = {
-  all: () => ['all'],
+  all: () => ['financeRecords'],
   lists: () => [...queryKeys.all(), 'lists'],
-  listByFilters: (args: { filters: FinanceRecordsSearchFilters }) => [...queryKeys.lists(), args.filters],
-  stats: (args: { filters: FinanceRecordsSearchFilters }) => [...queryKeys.all(), 'stats', args.filters],
+  listByFilters: (args: { filters: FinanceRecordsSearchFilters }) => [
+    ...queryKeys.lists(),
+    args.filters,
+  ],
+  stats: (args: { filters: FinanceRecordsSearchFilters }) => [
+    ...queryKeys.all(),
+    'stats',
+    args.filters,
+  ],
 }
 
 export const financeRecordQueryKeys = queryKeys

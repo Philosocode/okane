64e3b4a205c82db189dddbb75d5e78d6387de357
Tam Le <philosocode@gmail.com>
2025-01-12 19:46:27 -0700

    98-FE: Display finance record tags

---
+++ Okane.Client/src/shared/components/TagPill.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+type Props = {
+  tagName: string
+}
+
+const props = defineProps<Props>()
+</script>
+
+<template>
+  <div class="tag">{{ props.tagName }}</div>
+</template>
+
+<style scoped>
+.tag {
+  border: 1px solid var(--color-gray-100);
+  border-radius: 4px;
+  display: inline-block;
+  font-size: var(--font-size-sm);
+  min-width: 3rem;
+  padding-inline: 0.5rem;
+  text-align: center;
+}
+</style>

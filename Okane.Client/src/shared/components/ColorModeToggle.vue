899acb66b0f194515e9824da89c3aaad0a27f57c
Tam Le <philosocode@gmail.com>
2025-01-24 17:42:54 -0700

    113-FE: Add color mode toggle

---
+++ Okane.Client/src/shared/components/ColorModeToggle.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+// External
+import { computed } from 'vue'
+import { useDark } from '@vueuse/core'
+
+// Internal
+import { SHARED_COPY } from '@shared/constants/copy'
+
+const isDark = useDark({
+  storageKey: 'okane-color-mode',
+})
+
+const text = computed(() => {
+  if (isDark.value) return SHARED_COPY.ACTIONS.SWITCH_TO_LIGHT_MODE
+  return SHARED_COPY.ACTIONS.SWITCH_TO_DARK_MODE
+})
+
+function toggleColorMode() {
+  isDark.value = !isDark.value
+}
+</script>
+
+<template>
+  <button @click="toggleColorMode">{{ text }}</button>
+</template>

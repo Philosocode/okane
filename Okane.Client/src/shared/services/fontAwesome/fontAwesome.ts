c44cd3126f8cb0277f9d7f0d4a7bea079ea491d8
Tam Le <philosocode@gmail.com>
2025-01-30 17:32:29 -0700

    42-FE: Add more icons

--- Okane.Client/src/shared/services/fontAwesome/fontAwesome.ts
+++ Okane.Client/src/shared/services/fontAwesome/fontAwesome.ts
@@ -1,11 +1,31 @@
 // External
 import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUser as faUserRegular } from '@fortawesome/free-regular-svg-icons'
 import {
+  faDollarSign,
   faEllipsisVertical,
+  faMoon,
   faPenToSquare,
   faPlus,
+  faRightFromBracket,
+  faSun,
+  faTag,
   faTrash,
+  faUser,
   faXmark,
 } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faEllipsisVertical, faPenToSquare, faPlus, faTrash, faXmark)
+library.add(
+  faDollarSign,
+  faEllipsisVertical,
+  faMoon,
+  faPenToSquare,
+  faPlus,
+  faRightFromBracket,
+  faSun,
+  faTag,
+  faTrash,
+  faUser,
+  faUserRegular,
+  faXmark,
+)

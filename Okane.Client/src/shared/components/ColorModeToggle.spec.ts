4e625243474c856d24127e0a93430fa3d7a3bcef
Tam Le <philosocode@gmail.com>
2025-01-30 18:09:55 -0700

    42-FE: Re-design color mode toggle

--- Okane.Client/src/shared/components/ColorModeToggle.spec.ts
+++ Okane.Client/src/shared/components/ColorModeToggle.spec.ts
@@ -7,12 +7,20 @@ const mountComponent = getMountComponent(ColorModeToggle)
 
 test('renders a button to switch color modes', async () => {
   const wrapper = mountComponent()
+  const sunIconClass = '.fa-sun'
+  const moonIconClass = '.fa-moon'
+
   const button = wrapper.get('button')
   expect(button.text()).toBe(SHARED_COPY.ACTIONS.SWITCH_TO_DARK_MODE)
+  expect(button.find(sunIconClass).exists()).toBe(true)
 
   await button.trigger('click')
   expect(button.text()).toBe(SHARED_COPY.ACTIONS.SWITCH_TO_LIGHT_MODE)
+  expect(button.find(moonIconClass).exists()).toBe(true)
+
+  console.log(wrapper.html())
 
   await button.trigger('click')
   expect(button.text()).toBe(SHARED_COPY.ACTIONS.SWITCH_TO_DARK_MODE)
+  expect(button.find(sunIconClass).exists()).toBe(true)
 })

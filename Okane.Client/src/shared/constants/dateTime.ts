99c12930c94e210b787128cdd385e31878d6ee39
Tam Le <philosocode@gmail.com>
2025-02-24 23:44:26 -0700

    149-FE: Update common time format to use 1-12 hours
    Motivation: display 12AM instead of 24AM.

--- Okane.Client/src/shared/constants/dateTime.ts
+++ Okane.Client/src/shared/constants/dateTime.ts
@@ -4,7 +4,7 @@ import { SHARED_COPY } from '@shared/constants/copy'
 import { type SelectOption } from '@shared/components/form/FormSelect.vue'
 
 export const COMMON_DATE_FORMAT = 'MMM d yyyy'
-export const COMMON_TIME_FORMAT = 'k:mm a'
+export const COMMON_TIME_FORMAT = 'h:mm a'
 export const COMMON_DATE_TIME_FORMAT = `${COMMON_DATE_FORMAT} @ ${COMMON_TIME_FORMAT}`
 
 export enum TIME_INTERVAL {

3c97bc9382364c688580e5c012e306001f5c1faa
Tam Le <tam@philosocode.com>
2025-12-27 12:24:17 -0700

    174-FE: Add tests for `FINANCES_COPY.STATS.TOTAL_AMOUNT`

--- Okane.Client/src/features/financeRecords/constants/copy.spec.ts
+++ Okane.Client/src/features/financeRecords/constants/copy.spec.ts
@@ -29,3 +29,37 @@ describe('FINANCES_COPY.MONEY', () => {
     expect(result).toBe('+$99,999.90')
   })
 })
+
+describe('FINANCES_COPY.STATS.TOTAL_AMOUNT', () => {
+  test('returns the expected value when amount is 0', () => {
+    let result = FINANCES_COPY.STATS.TOTAL_AMOUNT({
+      amount: 0,
+      type: FINANCE_RECORD_TYPE.REVENUE,
+    })
+    expect(result).toBe('$0.00')
+
+    result = FINANCES_COPY.STATS.TOTAL_AMOUNT({
+      amount: 0,
+      type: FINANCE_RECORD_TYPE.EXPENSE,
+    })
+    expect(result).toBe('$0.00')
+  })
+
+  test('returns the expected value when type is expense', () => {
+    const amount = 99999.9
+    const result = FINANCES_COPY.STATS.TOTAL_AMOUNT({
+      amount,
+      type: FINANCE_RECORD_TYPE.EXPENSE,
+    })
+    expect(result).toBe('-$99,999.90')
+  })
+
+  test('returns the expected value when type is revenue', () => {
+    const amount = 99999.9
+    const result = FINANCES_COPY.STATS.TOTAL_AMOUNT({
+      amount,
+      type: FINANCE_RECORD_TYPE.REVENUE,
+    })
+    expect(result).toBe('+$99,999.90')
+  })
+})
